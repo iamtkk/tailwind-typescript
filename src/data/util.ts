5ad79ff1719f21d882b4a782df181c7171e953dc
김태관 <iamtk@neowiz.com>
2024-07-09 11:14:58 +0900

    luxon, chance 정의

--- src/data/util.ts
+++ src/data/util.ts
@@ -1,6 +1,6 @@
 export const makeArray = (length: number) => new Array(length).fill(null);
 
-// min과 max 사이의 값 크기의 array를 만들고 min 값을 기준으로 1씩 증가하는 값을 할당
+// min과 max 사이의 값 크기의 null로 채워진 array를 만들고, min 값을 기준으로 1씩 증가하는 값을 할당
 export const range = (min: number, max: number): number[] =>
   makeArray(max - min).map((notUsed, index) => index + min);
 
